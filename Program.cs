4e73e59cbd5f5f14dae25cc0e542362f939c4895
learningpath2025 <learningpath2025@outlook.com>
2025-09-03 11:43:29 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Assignment2._4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
